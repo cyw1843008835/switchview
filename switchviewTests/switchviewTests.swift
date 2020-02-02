8873b0524c49df1c2cb840e7167ee882dc54a3e7
chen <chen@gmsil.com>
2020-02-03 00:02:58 +0900

    Initial Commit

---
+++ switchviewTests/switchviewTests.swift
@@ -0,0 +1,34 @@
+//
+//  switchviewTests.swift
+//  switchviewTests
+//
+//  Created by yw c on 2020/02/03.
+//  Copyright © 2020 yw c. All rights reserved.
+//
+
+import XCTest
+@testable import switchview
+
+class switchviewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
